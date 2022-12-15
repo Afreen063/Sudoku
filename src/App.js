806e795fd61612b384dd8efe864b6dd196061749
Afreen063 <afreen0501@gmail.com>
2022-12-15 21:42:54 +0530

    input from the user has completed

--- src/App.js
+++ src/App.js
@@ -3,29 +3,65 @@ import { useState } from "react";
 
 
 function App() {
-   const [array, setArray]=useState([[1,2,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]]); 
+   const [array, setArray]=useState([
+    [null,null,null,null,null,null,null,null,null],
+    [null,null,null,null,null,null,null,null,null],
+    [null,null,null,null,null,null,null,null,null],
+    [null,null,null,null,null,null,null,null,null],
+    [null,null,null,null,null,null,null,null,null],
+    [null,null,null,null,null,null,null,null,null],
+    [null,null,null,null,null,null,null,null,null],
+    [null,null,null,null,null,null,null,null,null],
+    [null,null,null,null,null,null,null,null,null]
+   ]); 
    const [show, setShow] = useState(false)
    const handleInput=(e)=>{
-        
+        setShow(true);
+        console.log(array)
    } 
 
-   const handleClick=()=>{
-       setShow(true)
+   const handleChange=(row,col,event)=>{
+      let copy=[...array];
+      copy[row][col]=event.target.value;
+      setArray(copy);
    }
 
   return (
     <div className="App">
      <h1 className="text-3xl">Sudoku Solver</h1>
      <div className="table">
+     <table>
      <tbody>
          {
-         [0,3,6].map((i)=>{
+         array.map((row, rowIndex)=>{
           return (
-            <tr>
+            <tr key={rowIndex}>
               {
-                [0,3,6].map((j)=>{
+                row.map((col, colIndex)=>{
+                //  console.log(colIndex);
+                 if(colIndex==2||colIndex==5)
+                 {
+                 if(rowIndex==2||rowIndex==5)
+                 {
                   return (
-                    <td className="liItem">{array[i][j]}</td>
+                    <td key={colIndex} ><input type="Number" className="griditem bottomline" maxLength={1} onChange={e=>handleChange(rowIndex,colIndex,e)}></input></td>
+                  )
+                 }
+                 else{
+                  return (
+                    <td key={colIndex} ><input type="Number" className="griditem" maxLength={1} onChange={e=>handleChange(rowIndex,colIndex,e)}></input></td>
+                  )
+                 }
+                 }
+                  if(rowIndex==2||rowIndex==5)
+                 {
+                  return (
+                    <td key={colIndex} ><input type="Number" className="liItem bottomline" maxLength={1} onChange={e=>handleChange(rowIndex,colIndex,e)}></input></td>
+                  )
+                 }
+                 else
+                  return (
+                    <td key={colIndex}><input type="Number" className="liItem" maxLength={1} onChange={e=>handleChange(rowIndex,colIndex,e)}></input></td>
                   )
                 })
               }      
@@ -34,8 +70,10 @@ function App() {
          })
          }
          </tbody>
+     </table>
          </div>
-         <button onClick={handleClick}>Solve</button>
+         
+         <button onClick={handleInput}>Solve</button>
     </div>
   );
 }
